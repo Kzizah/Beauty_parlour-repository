83785c6208f0f72098ad0741e07487fa0775fe62
jelizasilatei <silateijc90@gmail.com>
2023-04-06 22:25:23 +0300

    a function that renders all services to the Dom and when clicked on displays the service details. fetch data from db.json update the style for servicesection

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ const serviceList = document.querySelector('#service-list ul');
 const serviceDetails = document.querySelector('#service__details');
 const imageDiv = document.querySelector('#service-image'); //This is the service image
 const priceDiv = document.querySelector('#service-price');
+const descriptionDiv = document.querySelector('#service-description');
 // const ticketDiv = document.querySelector('#film-tickets');
 const bookNowBtn = document.querySelector('#book-now');
-
+imageDiv.style.height ="100px";
+imageDiv.style.width ="100px";
 // Display Mobile Menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
@@ -18,33 +20,72 @@ const mobileMenu = () => {
 // Call the mobileMenu function
 menu.addEventListener('click', mobileMenu);
 
-getFirstFilm()
+getFirstService()
 initialize();
 //DOM renders 
 function renderOneService(service){
-   //Create list item for each film
+   //Create list item for each service
    let listItem = document.createElement('li');
-   let availableSlots = service.session_slots - service.booked_slots;
+   let availableSessionSlots = service.session_slots - service.booked_slots;
    listItem.innerText = `${service.service_name}`;
    listItem.addEventListener('click', () => {
-    //Display film Details
+    //Display service Details
     imageDiv.src = service.image;
-    priceDiv.textContent = `Service Price in $: ${service.price}`;
-    availableSlots.textContent = `Available Session Slots: ${slotsRemaining}`;
-      // Event listener for buy ticket button clicks
-  bookNowBtn.addEventListener("click", (e) =>{
+    priceDiv.textContent = `Service Price: ${service.price}`;
+    descriptionDiv.textContent = `Description: ${service.description}`
+    availableSessionSlots.textContent = `Available Session Slots: ${availableSessionSlots}`;
+      
+    // Event listener for buy ticket button clicks
+    bookNowBtn.addEventListener("click", (e) =>{
     e.preventDefault();
     console.log("boooked Session!");
-    slotsRemaining -= 1;
-    if(slotsRemaining <= 0){
-        availableSlots.textContent = `All Sessions Booked!` 
+    availableSessionSlots -= 1;
+    if(availableSessionSlots <= 0){
+        availableSessionSlots.textContent = `All Sessions Booked!` 
     }else{
-            availableSlots.textContent = `Available Session Slots : ${slotsRemaining}` 
+            availableSessionSlots.textContent = `Available Session Slots : ${availableSessionSlots}` 
     }
     
   });
    });
    serviceList.appendChild(listItem);
 }
+
+// Fetch all services
+function getAllServices(){
+    fetch('http://localhost:3000/services')
+    .then(res => res.json())
+    .then(services => services.forEach(service => {
+        renderOneService(service)
+        // updateAvailablesSlots(service)
+    }))
+}
+
+//Fetch Requests
+//Get Fetch for one service resource
+function getFirstService(){
+    fetch('http://localhost:3000/services/1')
+    .then(res => res.json())
+    .then(films => renderOneService(services[0]))
+}
+
+//Initialize Render- It will be the first thing that loads from our index js
+//Get Data and load/Render our films to the DOM 
+function initialize(){
+    getAllServices();
+}
+
+// Function to update the available tickets on the frontend
+function updateAvailableSlots(service, slotsBooked) {
+    // const ticketDiv = document.querySelector(`#film-tickets`);
+    // const availableTickets = film.capacity - film.tickets_sold;
+    // const newAvailableTickets = availableTickets - ticketsBought;
+    // if (newAvailableTickets < 0) {
+    //   // Can't buy more tickets than are available
+    //   return false;
+    // }
+    // ticketDiv.textContent = `Available Tickets: ${newAvailableTickets}`;
+    // return true;
+  }
 
 
