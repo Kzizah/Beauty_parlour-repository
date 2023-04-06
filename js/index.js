b5d3bc6c759d15905794e1447cbf46789cb086dd
jelizasilatei <silateijc90@gmail.com>
2023-04-06 21:19:19 +0300

    Insert data into my json file db.json

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Target our html with JavaScript
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const serviceList = document.querySelector('#service-list ul');
+const serviceDetails = document.querySelector('#service__details');
+const imageDiv = document.querySelector('#service-image'); //This is the service image
+const priceDiv = document.querySelector('#service-price');
+// const ticketDiv = document.querySelector('#film-tickets');
+const bookNowBtn = document.querySelector('#book-now');
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -11,3 +17,6 @@ const mobileMenu = () => {
 // Add an eventistener to toggle the two classes.
 // Call the mobileMenu function
 menu.addEventListener('click', mobileMenu);
+
+
+
